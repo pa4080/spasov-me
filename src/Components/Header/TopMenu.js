757cd002a83e58afbff6e63eb4252deb1790e2af
pa4080 <spas.z.spasov@gmail.com>
2022-06-21 01:27:21 +0300

    Tweak the menu

--- src/Components/Header/TopMenu.js
+++ src/Components/Header/TopMenu.js
@@ -1,11 +1,51 @@
+import { useState } from "react";
 import { Link } from 'react-router-dom';
 import "./topMenu.css";
 
 function TopMenu(props) {
+    const state = {
+        home: {
+            label: "Home",
+            link: "/home",
+            active: true
+        },
+        resume: {
+            label: "Resume",
+            link: "/resume",
+            active: false
+        },
+        switch(item) {
+            for (const member in this) {
+                if (typeof (this[member]) === "object") {
+                    if (this[member].label === item.label) {
+                        this[member].active = true;
+                    } else {
+                        this[member].active = false;
+                    }
+                }
+            }
+            setMenuItem({ ...this });
+        }
+    }
+    const [menuItems, setMenuItem] = useState(state);
+
+
     return (
         <nav className="mlt-header-top-container">
-            <Link to="/home" className="mlt-header-top-item">Home</Link>
-            <Link to="/resume" className="mlt-header-top-item">Resume</Link>
+            <Link
+                className={`mlt-header-top-item ${menuItems.home.active ? "mlt-header-top-link-active" : ""}`}
+                onClick={() => { state.switch(menuItems.home) }}
+                to={menuItems.home.link}
+            >
+                {menuItems.home.label}
+            </Link>
+            <Link
+                className={`mlt-header-top-item ${menuItems.resume.active ? "mlt-header-top-link-active" : ""}`}
+                onClick={() => { state.switch(menuItems.resume) }}
+                to={menuItems.resume.label}
+            >
+                {menuItems.resume.label}
+            </Link>
         </nav>
     )
 }
