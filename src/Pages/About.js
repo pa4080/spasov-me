0d04a5308403090979a25b160aff0e29835133c4
pa4080 <spas.z.spasov@gmail.com>
2022-06-29 12:12:25 +0300

    tidy up before the contacts section

--- src/Pages/About.js
+++ src/Pages/About.js
@@ -15,10 +15,10 @@ function About({ data, aboutTitleImage }) {
             <div className="mlt-about-container">
                 <Title photo={aboutTitleImage} data={data.title} />
                 <Section title="Resume" data={data.resume} component="Cell1x1Text" staticRows={1} />
-                <Section title="Edu­cation" data={data.education} component="Cell2x2" staticRows={1} tapToShow={true} />
-                <Section title="Employ­ment History" data={data.employmentHistory} component="Cell2x2" staticRows={1}  tapToShow={true} />
+                <Section title="Edu­cation" data={data.education} component="Cell2x2" staticRows={2} tapToShow={true} />
+                <Section title="Employ­ment History" data={data.employmentHistory} component="Cell2x2" staticRows={2}  tapToShow={true} />
                 <Section title="Spo­ken Lang­uag­es" data={data.languages} component="Cell1x3Stars" staticRows={data.languages.length} />
-                <Section title="IT Skills and Expe­rien­ce" data={data.skills} component="Cell1x3Stars" staticRows={7} tapToShow={true} />
+                <Section title="IT Skills and Expe­rien­ce" data={data.skills} component="Cell1x3Stars" staticRows={6} tapToShow={true} />
                 <div>&nbsp;</div>
             </div>
         </>
