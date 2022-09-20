a952c1793aa4b002969030a8ff14983a8ba7164a
pa4080 <spas.z.spasov@gmail.com>
2022-09-20 14:27:18 +0300

    About-Resume Section Update

--- src/Pages/About.js
+++ src/Pages/About.js
@@ -7,7 +7,7 @@ function About({ data, aboutTitleImage }) {
     return (
         <div className="mlt-about-container">
             <Title photo={aboutTitleImage} data={data.identity} />
-            <Section title="Resume" data={data.resume} component="Cell1x1Text" staticRows={1} />
+            <Section title="Resume" data={data.resume} component="Cell1x1Text" staticRows={2} />
             <Section title="Edu­cation" data={data.education} component="Cell2x2" staticRows={2} tapToShow={true} />
             <Section title="Employ­ment History" data={data.employmentHistory} component="Cell2x2" staticRows={2} tapToShow={true} />
             <Section title="Spo­ken Lang­uag­es" data={data.languages} component="Cell1x3Stars" staticRows={data.languages.length} />
