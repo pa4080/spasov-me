0ddef7d87cc4b83ada9258d00a7d51301a1d7690
Spas Z. Spasov <spas.z.spasov@metalevel.tech>
2023-06-20 17:11:39 +0300

    update according to the role in Browserbite

--- src/Pages/About.js
+++ src/Pages/About.js
@@ -1,20 +1,48 @@
 import Title from "../components/about-sections/Title";
-import Section from "../components/about-sections/Section"
+import Section from "../components/about-sections/Section";
 
 import "./about.css";
 
 function About({ data, aboutTitleImage }) {
-    return (
-        <div className="mlt-about-container">
-            <Title photo={aboutTitleImage} data={data.identity} />
-            <Section title="Resume" data={data.resume} component="Cell1x1Text" staticRows={2} />
-            <Section title="Edu­cation" data={data.education} component="Cell2x2" staticRows={2} tapToShow={true} />
-            <Section title="Employ­ment History" data={data.employmentHistory} component="Cell2x2" staticRows={1} tapToShow={true} />
-            <Section title="Spo­ken Lang­uag­es" data={data.languages} component="Cell1x3Stars" staticRows={data.languages.length} />
-            <Section title="IT Skills and Expe­rien­ce" data={data.skills} component="Cell1x3Stars" staticRows={6} tapToShow={true} />
-            {/* <p>&nbsp;</p> */}
-        </div>
-    );
+  return (
+    <div className="mlt-about-container">
+      <Title photo={aboutTitleImage} data={data.identity} />
+      <Section
+        title="Resume"
+        data={data.resume}
+        component="Cell1x1Text"
+        staticRows={3}
+      />
+      <Section
+        title="Edu­cation"
+        data={data.education}
+        component="Cell2x2"
+        staticRows={1}
+        tapToShow={true}
+      />
+      <Section
+        title="Employ­ment History"
+        data={data.employmentHistory}
+        component="Cell2x2"
+        staticRows={2}
+        tapToShow={true}
+      />
+      <Section
+        title="Spo­ken Lang­uag­es"
+        data={data.languages}
+        component="Cell1x3Stars"
+        staticRows={data.languages.length}
+      />
+      <Section
+        title="IT Skills and Expe­rien­ce"
+        data={data.skills}
+        component="Cell1x3Stars"
+        staticRows={6}
+        tapToShow={true}
+      />
+      {/* <p>&nbsp;</p> */}
+    </div>
+  );
 }
 
 export default About;
--- src/components/about-sections/Cell2x2.js
+++ src/components/about-sections/Cell2x2.js
@@ -1,52 +1,70 @@
 import React, { useState } from "react";
 import TapToShow from "./TapToShow";
-import './cell2x2.css';
+import "./cell2x2.css";
 
 function Cell2x2(props) {
-    const {
-        from, to, country, city, position, at, office, description
-    } = props.data;
-    const toYear = to.match(/(now|present)/) ? new Date().getFullYear() : to;
+  const { from, to, country, city, position, at, office, description } =
+    props.data;
+  const toYear = to.match(/(now|present)/) ? new Date().getFullYear() : to;
 
-    const [isExpanded, setIsExpanded] = useState(false);
+  const [isExpanded, setIsExpanded] = useState(false);
 
-    const expandSection = () => {
-        isExpanded ? setIsExpanded(false) : setIsExpanded(true);
-    };
+  const expandSection = () => {
+    isExpanded ? setIsExpanded(false) : setIsExpanded(true);
+  };
 
-    return (
-        <div className={`mlt-cell-2x2-grid ${props.tapToShow ? "mlt-tapToShow" : ""}`}>
-            <div className="mlt-cell-2x2-col-1">
-                <p className="mlt-cell-2x2-col-1-row-1">
-                    <span>{from} - {toYear}</span>
-                </p>
-                <p className="mlt-cell-2x2-col-1-row-2">
-                    {country ? <span>{city}, {country}</span> : null}
-                </p>
-                {(props.tapToShow && description)
-                    ? <TapToShow expandSection={expandSection} isExpanded={isExpanded} />
-                    : null
-                }
-            </div>
-            <div className="mlt-cell-2x2-col-2">
-                <p className="mlt-cell-2x2-col-2-row-1">
-                    {position ? <span className="mlt-cell-2x2-col-2-row-1-position">{position}</span> : null}
-                    {position && office && at ? <span>{at}</span> : null}
-                    {office ? <span className="mlt-cell-2x2-col-2-row-1-position-at">{office}.</span> : "."}
-                </p>
-                
-                {description
-                    ? description.map((line, index) => 
-                        <p key={`${line.length}.${index}`}
-                            className={`mlt-cell-2x2-col-2-row-2 mlt-tapToShow-${isExpanded ? "expanded" : "collapsed"}`}              
-                        > 
-                            {line}
-                        </p>
-                    ) : null
-                }
-            </div>
-        </div>
-    )
+  return (
+    <div
+      className={`mlt-cell-2x2-grid ${props.tapToShow ? "mlt-tapToShow" : ""}`}
+    >
+      <div className="mlt-cell-2x2-col-1">
+        <p className="mlt-cell-2x2-col-1-row-1">
+          <span>
+            {from} - {toYear}
+          </span>
+        </p>
+        <p className="mlt-cell-2x2-col-1-row-2">
+          {country ? (
+            <span>
+              {city}, {country}
+            </span>
+          ) : null}
+        </p>
+        {props.tapToShow && description ? (
+          <TapToShow expandSection={expandSection} isExpanded={isExpanded} />
+        ) : null}
+      </div>
+      <div className="mlt-cell-2x2-col-2">
+        <p className="mlt-cell-2x2-col-2-row-1">
+          {position ? (
+            <span className="mlt-cell-2x2-col-2-row-1-position">
+              {position}
+            </span>
+          ) : null}
+          {position && office && at ? <span>{at}</span> : null}
+          {office ? (
+            <span className="mlt-cell-2x2-col-2-row-1-position-at">
+              {office}.
+            </span>
+          ) : (
+            "."
+          )}
+        </p>
+
+        {description
+          ? description.map((line, index) => (
+              <p
+                key={`${line.length}.${index}`}
+                className={`mlt-cell-2x2-col-2-row-2 mlt-tapToShow-${
+                  isExpanded ? "expanded" : "collapsed"
+                }`}
+                dangerouslySetInnerHTML={{ __html: line }}
+              />
+            ))
+          : null}
+      </div>
+    </div>
+  );
 }
 
 export default Cell2x2;
