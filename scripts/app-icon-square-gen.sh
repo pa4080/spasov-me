1cf4b66809984e274f40d86d15908e71ef46b2a0
Spas Z. Spasov <spas.z.spasov@metalevel.tech>
2023-08-29 16:56:16 +0300

    feat: rebuild the icons to exact square

--- scripts/app-icon-square-gen.sh
+++ scripts/app-icon-square-gen.sh
@@ -16,15 +16,18 @@ FAVICON_SVG_DEST_DIRECTORY="public/"
 # Generate app-icons
 for res in 192 512; do
   convert -background transparent -resize "${res}x${res}" \
+    -background transparent -gravity center -extent "${res}x${res}" \
     "$LOGO_SOURCE_FILE" "${LOGO_DEST_DIRECTORY}/app-icon-${res}.png"
 done
 
 for res in 114 120 144 152 180 57 60 72 76 36; do
   convert -background transparent -resize "${res}x${res}" \
+    -background transparent -gravity center -extent "${res}x${res}" \
     "$LOGO_SOURCE_FILE" "${LOGO_DEST_DIRECTORY}/apple-touch-icon-${res}x${res}.png"
 done
 
 convert -background transparent "$FAVICON_SOURCE_FILE" -clone 0 -resize "64x64" \
+  -background transparent -gravity center -extent "64x64" \
   "${FAVICON_ICO_DEST_DIRECTORY}/favicon.ico"
 
 cp "$FAVICON_SOURCE_FILE" "${FAVICON_SVG_DEST_DIRECTORY}/favicon.svg"
