e320301e7997938a24524ed60afc45985f72e9ff
pa4080 <spas.z.spasov@metalevel.tech>
2023-11-10 14:38:12 +0200

    feat: add @tailwindcss/forms @see https://www.npmjs.com/package/@tailwindcss/forms

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -16,6 +16,24 @@ module.exports = {
       },
     },
     extend: {
+      screens: {
+				"6xs": "320px",
+				"5xs": "360px",
+				"4xs": "380px",
+				"3xs": "420px",
+				"2xs": "480px",
+				xs: "520px", // extra small
+				xa: "580px", // extra small average
+				sm: "640px",
+				sa: "720px", // small average
+				md: "768px",
+				ma: "820px",
+				ml: "920px",
+				lg: "1024px",
+				xl: "1280px",
+				"1xl": "1366px",
+				"2xl": "1536px",
+			},
       colors: {
         border: "hsl(var(--border))",
         input: "hsl(var(--input))",
@@ -69,12 +87,28 @@ module.exports = {
           from: { height: "var(--radix-accordion-content-height)" },
           to: { height: 0 },
         },
+				fadeIn: {
+					"0%": { opacity: "0%" },
+					"20%": { opacity: "0%" },
+					"100%": { opacity: "100%" },
+				},
+				fadeInSiderTextDark: {
+					"0%": { opacity: "0%" },
+					"100%": { opacity: "35%" },
+				},
+				fadeInSiderTextLight: {
+					"0%": { opacity: "0%" },
+					"100%": { opacity: "5%" },
+				},
       },
       animation: {
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
+        fadeIn: "fadeIn 2.5s ease-in-out forwards",
+				fadeInSiderTextDark: "fadeInSiderTextDark 2.5s ease-in-out forwards",
+				fadeInSiderTextLight: "fadeInSiderTextLight 2.5s ease-in-out forwards",
       },
     },
   },
-  plugins: [require("tailwindcss-animate")],
+  plugins: [require("tailwindcss-animate"), require("@tailwindcss/forms")],
 }
