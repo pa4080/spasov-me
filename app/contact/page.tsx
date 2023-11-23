b6705bebb1fd71b102f05a04dbf7834d5f15c3ba
pa4080 <spas.z.spasov@metalevel.tech>
2023-11-23 15:42:40 +0200

    feat: style the contact form

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,9 +1,15 @@
 import Contact from "@/components/contact";
-import ThemeSwitch from "@/components/layouts/theme/ThemeSwitch";
 import { msgs } from "@/messages";
 
 const Home: React.FC = () => {
-	return <Contact />;
+	const t = msgs("Contact");
+
+	return (
+		<div className="margin_vh_top margin_vh_bottom">
+			<h1 className="section_title">{t("title")}</h1>
+			<Contact className="bg-secondary px-5 py-3 rounded-2xl" />
+		</div>
+	);
 };
 
 export default Home;
