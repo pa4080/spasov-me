4f0c064c58b54b22d9bf9059bccd7e55bf84df53
pa4080 <spas.z.spasov@metalevel.tech>
2023-11-27 12:11:36 +0200

    docs: play with middleware.ts

---
+++ app/contact/_layout.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import RecaptchaContextProvider from "@/contexts/RecaptchaContextProvider";
+
+interface ReCaptchaLayout {
+	children: React.ReactNode;
+}
+
+const ReCaptchaLayout: React.FC<ReCaptchaLayout> = ({ children }) => {
+	return <RecaptchaContextProvider>{children}</RecaptchaContextProvider>;
+};
+
+export default ReCaptchaLayout;
--- middleware.ts
+++ middleware.ts
@@ -2,8 +2,24 @@
  * @see https://next-auth.js.org/configuration/nextjs#prerequisites
  */
 
+/**
+ *
+ import { Route } from "@/routes";
+
+ const publicRoutes = Object.keys(Route.public)
+	.map((key) => {
+		const route = Route.public[key as keyof typeof Route.public];
+
+		if (route.visible) {
+			return route.uri.slice(1);
+		}
+	})
+	.filter((route) => route)
+	.join("|");
+*/
+
 export { default } from "next-auth/middleware";
 
 export const config = {
-	matcher: ["/((?!api|_next|.*\\..*|$|portfolio|cv|contact|home).*)"],
+	matcher: [`/((?!api|_next|.*\\..*|$|home|about|contact).*)`],
 };
