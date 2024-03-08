0d7e873e487dbf9827078ca4e045d34748b807d0
pa4080 <spas.z.spasov@metalevel.tech>
2024-03-08 10:48:12 +0200

    feat: edit projects from the public interface when logged in

--- contexts/AppContext.tsx
+++ contexts/AppContext.tsx
@@ -13,12 +13,14 @@ import { Session } from "next-auth";
 import { getProviders, useSession } from "next-auth/react";
 
 import { getEntries } from "@/components/about/_about.actions";
-import { getFiles } from "@/components/files/_files.actions";
+import { getFileList, getFiles } from "@/components/files/_files.actions";
 import { getPageCards } from "@/components/pages/_pages.actions";
+import { getProjects } from "@/components/portfolio/_portfolio.actions";
 import { getTags } from "@/components/tags/_tags.actions";
 import { AboutEntryData } from "@/interfaces/AboutEntry";
-import { FileData } from "@/interfaces/File";
+import { FileData, FileListItem } from "@/interfaces/File";
 import { PageCardData } from "@/interfaces/PageCard";
+import { ProjectData } from "@/interfaces/Project";
 import { TagData } from "@/interfaces/Tag";
 import { AuthProviders } from "@/types/next-auth-providers";
 
@@ -29,10 +31,14 @@ interface AppContextProps {
 	setAboutEntries: Dispatch<SetStateAction<AboutEntryData[]>>;
 	files: FileData[];
 	setFiles: Dispatch<SetStateAction<FileData[]>>;
+	fileList: FileListItem[];
+	setFileList: Dispatch<SetStateAction<FileListItem[]>>;
 	pages: PageCardData[];
 	setPages: Dispatch<SetStateAction<PageCardData[]>>;
 	tags: TagData[];
 	setTags: Dispatch<SetStateAction<TagData[]>>;
+	projects: ProjectData[];
+	setProjects: Dispatch<SetStateAction<ProjectData[]>>;
 }
 
 const AppContext = createContext<AppContextProps>({} as AppContextProps);
@@ -45,8 +51,10 @@ export const AppContextProvider: React.FC<AppContextProviderProps> = ({ children
 	const [authProviders, setAuthProviders] = useState<AuthProviders>(null);
 	const [aboutEntries, setAboutEntries] = useState<AboutEntryData[]>([]);
 	const [files, setFiles] = useState<FileData[]>([]);
+	const [fileList, setFileList] = useState<FileListItem[]>([]);
 	const [pages, setPages] = useState<PageCardData[]>([]);
 	const [tags, setTags] = useState<TagData[]>([]);
+	const [projects, setProjects] = useState<ProjectData[]>([]);
 
 	const { data: session } = useSession();
 
@@ -56,28 +64,15 @@ export const AppContextProvider: React.FC<AppContextProviderProps> = ({ children
 
 			setAboutEntries((await getEntries({ hyphen: true, public: true })) ?? []);
 			setFiles((await getFiles({ hyphen: true, public: true })) ?? []);
+			setFileList((await getFileList()) ?? []);
 			setPages((await getPageCards({ hyphen: true, public: true })) ?? []);
 			setTags((await getTags({ hyphen: true, public: true })) ?? []);
+			setProjects((await getProjects({ hyphen: true, public: true })) ?? []);
 		})();
 
 		return () => {};
 		// eslint-disable-next-line react-hooks/exhaustive-deps
 	}, []);
-
-	// useEffect(() => {
-	// 	// eslint-disable-next-line no-console
-	// 	console.log("FILES", files);
-	// }, [files]);
-
-	// useEffect(() => {
-	// 	// eslint-disable-next-line no-console
-	// 	console.log("PAGES", pages);
-	// }, [pages]);
-
-	// useEffect(() => {
-	// 	// eslint-disable-next-line no-console
-	// 	console.log("TAGS", tags);
-	// }, [tags]);
 
 	return (
 		<AppContext.Provider
@@ -88,10 +83,14 @@ export const AppContextProvider: React.FC<AppContextProviderProps> = ({ children
 				setAboutEntries,
 				files,
 				setFiles,
+				fileList,
+				setFileList,
 				pages,
 				setPages,
 				tags,
 				setTags,
+				projects,
+				setProjects,
 			}}
 		>
 			{children}
