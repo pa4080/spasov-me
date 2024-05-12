7b02c9523c32be65467cbbf15b4483081a08484c
pa4080 <spas.z.spasov@metalevel.tech>
2024-05-12 12:53:48 +0300

    feat: show gray generic logo when a project doesn't gave one attached

--- components/fragments/Gallery/GalleryCarouselNavInProject.tsx
+++ components/fragments/Gallery/GalleryCarouselNavInProject.tsx
@@ -87,8 +87,15 @@ const GalleryCarouselNavInProject: React.FC<Props> = ({
 						src={
 							project.html.icon?.metadata.html.fileUrl ||
 							project.html.icon?.metadata.html.fileUri ||
-							Route.assets.IMAGE_PLACEHOLDER
+							Route.assets.LOGO_SVG
 						}
+						style={{
+							filter:
+								!project.html.icon?.metadata.html.fileUrl &&
+								!project.html.icon?.metadata.html.fileUri
+									? "grayscale(1)"
+									: "",
+						}}
 						unoptimized={project.html.icon?.filename.match(/\.svg$/) ? true : false}
 						width={44}
 					/>
