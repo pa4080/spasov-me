6cea3479e6244a7d402d74d121590ee305e64c95
pa4080 <spas.z.spasov@metalevel.tech>
2024-06-05 13:19:48 +0300

    feat: embed MSOffice and PDF files

--- app/embed-test/page.tsx
+++ app/embed-test/page.tsx
@@ -1,18 +1,21 @@
+import EmbedMsoDoc from "@/components/fragments/embed/EmbedMsoDoc";
+import EmbedPdfDoc from "@/components/fragments/embed/EmbedPdfDoc";
 import React from "react";
 
-// import dynamic from "next/dynamic";
-// const SearchPublic = dynamic(() => import("@/components/search/public"));
-
-const sourceUrl: string = "https://media.spasov.me/spas.spasov_accessKeys_aws.xlsx";
-const baseUrl: string = "https://view.officeapps.live.com/op/";
-const action: "view" | "embed" = "view";
-
-const Portfolio: React.FC = async () => {
-	const srcUrl = baseUrl + action + ".aspx?src=" + sourceUrl;
+const Portfolio: React.FC = () => {
+	const srcUrl: string = "https://media.spasov.me/spas.spasov_accessKeys_aws.xlsx";
 
 	return (
-		<div className="margin_vh_top margin_vh_bottom scroll-m-40 h-72">
-			<iframe height="100.4%" width="100.4%" src={srcUrl} />
+		<div className="margin_vh_top margin_vh_bottom scroll-m-40 space-y-8">
+			<div style={{ width: "100%", height: "520px" }} className="rounded-md overflow-hidden">
+				<EmbedMsoDoc sourceUrl={srcUrl} style={{ height: "100.2%", width: "100.2%" }} />
+			</div>
+			<div style={{ width: "100%", height: "520px" }} className="rounded-md overflow-hidden">
+				<EmbedPdfDoc
+					sourceUrl="https://media.spasov.me/JOBS_English-2024-05-30.pdf?v=1717581660000"
+					style={{ height: "100.2%", width: "100.2%" }}
+				/>
+			</div>
 		</div>
 	);
 };
