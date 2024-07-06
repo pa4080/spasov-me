e0cd5f09951a8bddb0d139eac6c34c01a367959e
pa4080 <spas.z.spasov@metalevel.tech>
2024-07-07 00:23:25 +0300

    refactor: use "force-dynamic"

--- app/admin/tags/page.tsx
+++ app/admin/tags/page.tsx
@@ -2,6 +2,10 @@ import React from "react";
 
 import TagsAdmin from "@/components/tags/admin";
 
+//nextjs.org/docs/messages/app-static-to-dynamic-error
+// export const dynamic = 'force-static';
+export const dynamic = "force-dynamic";
+
 const Pages: React.FC = () => {
 	return <TagsAdmin className="margin_vh_top margin_vh_bottom scroll-m-40" />;
 };
