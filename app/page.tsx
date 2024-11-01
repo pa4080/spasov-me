c32fc532016855f4791eca4f09dae193391b2f55
pa4080 <spas.z.spasov@metalevel.tech>
2024-11-01 22:24:58 +0200

    fix: short pages (height and) footer position

--- app/page.tsx
+++ app/page.tsx
@@ -8,14 +8,14 @@ const Home: React.FC = async () => {
   const t = msgs("Home");
 
   return (
-    <>
+    <div className="min-h-contentShortPage">
       <SiteLogo
         className="w-full margin_vh_top"
         greeting_ln1={t("greeting_ln1")}
         greeting_ln2={t("greeting_ln2")}
       />
       <PagesPublic className="margin_vh_middle margin_vh_bottom" />
-    </>
+    </div>
   );
 };
 
--- app/search/page.tsx
+++ app/search/page.tsx
@@ -43,7 +43,7 @@ const Portfolio: React.FC = async () => {
   }));
 
   return (
-    <div className="margin_vh_top margin_vh_bottom scroll-mt-40">
+    <div className="margin_vh_top scroll-mt-40 min-h-contentShortPage">
       <h1 className="section_title">{t("title")}</h1>
       <SearchPublic {...data} />
     </div>
