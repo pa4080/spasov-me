9674d7fc3ac7c3fd451528f412c3f2784cda4e56
pa4080 <spas.z.spasov@metalevel.tech>
2024-11-02 12:44:03 +0200

    fix: tags cards style

--- components/tags/admin/Card/index.tsx
+++ components/tags/admin/Card/index.tsx
@@ -3,11 +3,11 @@ import React from "react";
 import DisplayIcon from "@/components/fragments/DisplayIcon";
 import { type IconsMap } from "@/interfaces/IconsMap";
 import { type TagData } from "@/interfaces/Tag";
+import { cn } from "@/lib/cn-utils";
 import { msgs } from "@/messages";
 
 import DeleteTag from "../Actions/DeleteTag";
 import UpdateTag from "../Actions/UpdateTag";
-import styles from "./_tag-card.module.css";
 
 export interface GenericActionProps {
   className?: string;
@@ -28,36 +28,41 @@ const TagCard: React.FC<GenericActionProps> = ({ tag, className, iconsMap }) => 
   const tForm = msgs("Tags_Form");
 
   return (
-    <div className={`${styles.cardWrapper} ${className}`} id={`tag_${tag._id}`}>
-      <div className={styles.card}>
-        <div className={styles.buttons}>
+    <div
+      className={cn("scroll-mt-24 3xl:scroll-mt-8 relative group", className)}
+      id={`tag_${tag._id}`}
+    >
+      <div
+        className={cn(
+          "card-shadow",
+          "bg-card/[50%] md:bg-card/[25%] dark:bg-card/[25%] rounded-2xl h-full p-4 min-h-fit border-4 border-transparent",
+          "hover:bg-popover/[80%] hover:border-muted/60 dark:hover:border-primary/60",
+          "group-target:border-primary/80",
+          "grid grid-cols-[7rem_1fr] gap-y-2"
+        )}
+      >
+        <div className="flex gap-2 absolute right-0 bottom-0 p-4 rounded-2xl">
           <DeleteTag tag={tag} />
           <UpdateTag iconsMap={iconsMap} tag={tag} tagType={tag.tagType} />
         </div>
 
-        <div className={styles.cardRow}>
-          <div className={styles.leftCol}>{t("name")}:</div>
-          <div className={`${styles.rightCol} font-semibold`}>{name}</div>
+        <div className="">{t("name")}:</div>
+        <div className="line-clamp-1 font-semibold">{name}</div>
+
+        <div className="">{t("description")}:</div>
+        <div dangerouslySetInnerHTML={{ __html: description }} className="line-clamp-1" />
+
+        <div className="">{t("type")}:</div>
+        <div className="line-clamp-1">
+          {(tForm("tag_type_list") as unknown as Record<string, string>)[tagType]}
         </div>
-        <div className={styles.cardRow}>
-          <div className={styles.leftCol}>{t("description")}:</div>
-          <div dangerouslySetInnerHTML={{ __html: description }} className={styles.rightCol} />
-        </div>
-        <div className={styles.cardRow}>
-          <div className={styles.leftCol}>{t("type")}:</div>
-          <div className={styles.rightCol}>
-            {(tForm("tag_type_list") as unknown as Record<string, string>)[tagType]}
-          </div>
-        </div>
-        <div className={styles.cardRow}>
-          <div className={styles.leftCol}>{t("orderKey")}:</div>
-          <div className={styles.rightCol}>{orderKey}</div>
-        </div>
-        <div className={styles.cardRow}>
-          <div className={styles.leftCol}>{t("icon")}:</div>
-          <div className={styles.rightCol}>
-            <DisplayIcon icon={iconsMap[icon]} />
-          </div>
+
+        <div className="">{t("orderKey")}:</div>
+        <div className="line-clamp-1">{orderKey}</div>
+
+        <div className="">{t("icon")}:</div>
+        <div className="line-clamp-1">
+          <DisplayIcon icon={iconsMap[icon]} />
         </div>
       </div>
     </div>
