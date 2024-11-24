b63ac920762c09e5a4bf40b04347e177324c316c
pa4080 <spas.z.spasov@metalevel.tech>
2024-11-24 12:36:37 +0200

    refactor: the backup script

--- scripts/dump-mongo-db.sh
+++ scripts/dump-mongo-db.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
-# run it fron the root of the project: tmp/dump-mongo-db.sh .env.local
+# run it from the root of the project: scripts/dump-mongo-db.sh .env.local
 
 if [ -f "$1" ]; then . "$1"; fi
 
 source_db_cs="${MONGODB_URI}"
 destin_db_cs="${MONGODB_URI_LOCAL}"
+destin_db_bk="${MONGODB_URI_BACKUP}"
 
-if [ -z "$source_db_cs" ] || [ -z "$destin_db_cs" ]; then
+if [ -z "$source_db_cs" ] || [ -z "$destin_db_cs" ] || [ -z "$destin_db_bk" ]; then
 	echo "Missing env vars: MONGODB_URI, MONGODB_URI_LOCAL"
 	exit 1
 fi
@@ -16,6 +17,10 @@ rm -rf "$dumppath"/dbbackup && mkdir -p "${dumppath}/dbbackup"
 
 mongodump --uri "$source_db_cs" --out="$dumppath"/dbbackup
 mongorestore --uri "$destin_db_cs" "$dumppath"/dbbackup --drop --authenticationDatabase=admin
+mongorestore --uri "$destin_db_bk" "$dumppath"/dbbackup --drop --authenticationDatabase=admin
 
-# rm -rf "$dumppath"/dbbackup
+if [ ! -z "$2" ] && [ "$2" == "rm-dump" ]; then
+	rm -rf "$dumppath"/dbbackup
+fi
+
 exit
