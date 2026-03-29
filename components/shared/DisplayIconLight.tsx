6ac25bfc6366b0c18da2f7632ae0434c3e8e6a1f
Spas Z. Spasov <spas.z.spasov@metalevel.tech>
2026-03-29 12:18:06 +0300

    fix: homepage icon size warning

--- components/shared/DisplayIconLight.tsx
+++ components/shared/DisplayIconLight.tsx
@@ -1,6 +1,6 @@
 "use client";
-import React from "react";
 import Image from "next/image";
+import React from "react";
 
 import { type IconsMap } from "@/interfaces/IconsMap";
 
@@ -12,16 +12,24 @@ interface Props {
   theme?: "light" | "dark";
 }
 
+/**
+ * For some reason the <Image/> component produces the following error for unoptimized SVGs here:
+ * Image with src "https://..." has either width or height modified, but not the other.
+ * If you use CSS to change the size of your image, also include the styles 'width: "auto"'
+ * or 'height: "auto"' to maintain the aspect ratio.
+ */
 const DisplayIconLight: React.FC<Props> = ({ icon, width = 48, height = 48, theme = "light" }) => {
+  const src =
+    theme === "dark" ? icon?.uri?.dark || icon?.uri?.light : icon?.uri?.light || icon?.uri?.dark;
+  const isSvg = /\.svg(?:\?v=.*$)/.exec(src || "") ? true : false;
+  const Component = isSvg ? "img" : Image;
+
   return (
-    <Image
-      priority
+    <Component
+      {...(isSvg ? {} : { priority: true, unoptimized: true })}
       alt={icon?.name || "Icon"}
       height={height}
-      src={
-        theme === "dark" ? icon?.uri?.dark || icon?.uri?.light : icon?.uri?.light || icon?.uri?.dark
-      }
-      unoptimized={true}
+      src={src}
       width={width}
     />
   );
